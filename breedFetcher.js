da337599dc4937d1c0f86073c84925238b8d6f54
k2pbac <krisbachan123@hotmail.com>
2021-08-26 14:23:29 -0400

    add mocha and chai for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,19 @@
 const request = require("request");
 
 
-request(`https://api.thecatapi.com/v1/images/search?breed_ids=${process.argv.slice(2)[0].slice(0,4)}`, (error, response, body) => {
-  let newBody = JSON.parse(body, null, 2);
-  if (typeof newBody !== 'undefined' && newBody.length) {
-    console.log(newBody[0]);
-  } else {
-    console.log("Sorry we could not find that breed of cat!");
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName.slice(0,4)}`, (error, response, body) => {
+    let newBody = JSON.parse(body, null, 2);
+    if (newBody[0].breeds.length) {
+      return callback(null, newBody[0].breeds[0].description);
+    } else {
+      return callback(error, null);
+    }
+  });
+};
+
+
+module.exports = {
+  fetchBreedDescription
+};
